ab79e6f32bcf7ea1f948c6505494986bec2b7f92
Aleksandr Shilov <alex.shilov.by@gmail.com>
2025-04-22 00:17:51 +0300

    Add missing test
    Signed-off-by: Aleksandr Shilov <alex.shilov.by@gmail.com>

--- e2e/cli_decrypt_test.go
+++ e2e/cli_decrypt_test.go
@@ -11,7 +11,7 @@ import (
 func TestCliDecrypt(t *testing.T) {
 	t.Parallel()
 
-	t.Run("it works", func(t *testing.T) {
+	t.Run("it works (signature from file)", func(t *testing.T) {
 		t.Parallel()
 		cli, _ := MustSetupCli(t)
 		encrypted := MustReadAll(t, "testdata/message_super_v1_00.enc")
@@ -23,6 +23,24 @@ func TestCliDecrypt(t *testing.T) {
 			WithStdout(stdout).
 			WithStderr(stderr).
 			Exec("decrypt", "-i", "testdata/super_v1_00.priv", "-p", "testdata/super_v1_00.pub", "--sig", "testdata/message_super_v1_00.enc.sig")
+		require.NoError(t, err)
+		require.Empty(t, stderr)
+		require.Equal(t, "message", stdout.String())
+	})
+
+	t.Run("it works (signature from hex)", func(t *testing.T) {
+		t.Parallel()
+		cli, _ := MustSetupCli(t)
+		encrypted := MustReadAll(t, "testdata/message_super_v1_00.enc")
+		sig := MustReadAll(t, "testdata/message_super_v1_00.enc.sig")
+		stdout := new(bytes.Buffer)
+		stderr := new(strings.Builder)
+
+		err := cli.
+			WithStdin(bytes.NewReader(encrypted)).
+			WithStdout(stdout).
+			WithStderr(stderr).
+			Exec("decrypt", "-i", "testdata/super_v1_00.priv", "-p", "testdata/super_v1_00.pub", "--sig-hex", string(sig))
 		require.NoError(t, err)
 		require.Empty(t, stderr)
 		require.Equal(t, "message", stdout.String())
